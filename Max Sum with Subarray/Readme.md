ebcec478c9980093f73ca65a29715f6e827f8244
Mr. Twinkle Sharma <smtwinkle451@gmail.com>
2020-11-15 16:56:44 +0530

    2nd Question Uploaded

---
+++ Max Sum with Subarray/Readme.md
@@ -0,0 +1,26 @@
+# Max Sum with Subarray
+  
+
+
+ 
+### Constraints
+### Input Format 
+```
+First Line will Contains Number of t testcases 
+Second Line Contains length of array n 
+Third Line will contain space separated n numbers 
+```
+
+### Output Format 
+```
+Output the Maximum Sum which can be obtained from this array 
+```
+
+### Sample Input 1
+
+### Sample Output 1
+
+### Sample Input 2
+
+
+### Sample Output 2
---
+++ Max Sum with Subarray/maxsub.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main()
+{
+	int n;
+    cin>>n;
+    vector<ll>a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    ll curr=a[0];
+    ll total=0;
+    int flag=0;
+    for(int i=1;i<n;i++){
+        if(flag==0){
+            if(a[i]>a[i-1])
+                curr=a[i];
+            else{
+                total+=a[i-1];
+                curr=a[i];
+                flag=1;
+            }
+        }
+        else{
+            if(a[i]<a[i-1])
+                curr=a[i];
+            else{
+                total-=a[i-1];
+                curr=a[i];
+                flag=0;
+            }
+        }
+        
+	}
+	cout<<total;
+	return 0;
+}
+

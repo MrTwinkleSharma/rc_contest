cfb495232d9165b33e13c0492d979d74feabbd77
Mr. Twinkle Sharma <smtwinkle451@gmail.com>
2020-11-18 04:36:09 +0530

    successful Implementation

--- Businees&War/busnwar.cpp
+++ Businees&War/busnwar.cpp
@@ -7,11 +7,12 @@ char ArrayMatrix[10001][10001];
 int move_in_x[] = {-1,0,1,0};
 int move_in_y[] = {0,1,0,-1};
 
+
 bool is_Valid_Move(int x, int y)
 {
 	if (x<1 || x>m || y<1 || y>n) return false;
 	
-	if(visited[x][y] == true || ArrayMatrix[x][y]== 'G') return false;
+	if(visited[x][y] == true || ArrayMatrix[x][y]== '#') return false;
 	
 	return true;
 }
@@ -21,10 +22,9 @@ void DFS(int x, int y)
 	visited[x][y] = true;
 	for(int i=0;i<4;i++)
 	{
-		if(is_Valid_Move(x+move_in_x[i], move_in_y[i]))
-			DFS((x+move_in_x[i], move_in_y[i]);	
+		if(is_Valid_Move(x + move_in_x[i], y + move_in_y[i]))
+			DFS(x + move_in_x[i], y+ move_in_y[i]);	
 	}
-	
 }
 
 int main()
@@ -40,7 +40,7 @@ int main()
 	for(int i=1;i<=m;i++)
 	{
 		for(int j=1;j<=n;j++)
-		if(ArrayMatrix[i][j] =='P' && visited[i][j] == false)
+		if(ArrayMatrix[i][j] =='.' && visited[i][j] == false)
 		{
 			DFS(i,j);
 			count++;			

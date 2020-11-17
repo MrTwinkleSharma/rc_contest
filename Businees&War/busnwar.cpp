e9fe8546864527661fd5dc42bb51ad44243a2ba5
Mr. Twinkle Sharma <smtwinkle451@gmail.com>
2020-11-18 04:00:34 +0530

    Update busnwar.cpp

--- Businees&War/busnwar.cpp
+++ Businees&War/busnwar.cpp
@@ -1,29 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool is_Valid_Move(int x, int y, char ArrayMatrix[][] , bool visited[][])
+bool is_Valid_Move(int m, int n, int x, int y, char ArrayMatrix[][] , bool visited[][])
 {
-	if (x<1 || x>N || y<1 || y>M) return false;
+	if (x<1 || x>m || y<1 || y>n) return false;
 	
 	if(visited[x][y] == true || ArrayMatrix[x][y]== 'G') return false;
 	
 	return true;
 }
 
-void DFS(int x, int y, char ArrayMatrix[][] , bool visited[][])
+void DFS(int m, int n, int x, int y, char ArrayMatrix[][] , bool visited[][])
 {
 	visited[x][y] = true;
-	if(is_Valid_Move(x-1,y,ArrayMatrix,visited))
-	DFS(x-1,y);
+	if(is_Valid_Move(m,n,x-1,y,ArrayMatrix,visited))
+	DFS(m,n,x-1,y,ArrayMatrix,visited);
 	
-	if(is_Valid_Move(x,y+1,ArrayMatrix,visited))
-	DFS(x,y+1);
+	if(is_Valid_Move(m,n,x,y+1,ArrayMatrix,visited))
+	DFS(m,n,x,y+1,ArrayMatrix,visited);
 	
-	if(is_Valid_Move(x+1,y,ArrayMatrix,visited))
-	DFS(x+1,y));
+	if(is_Valid_Move(m,n,x+1,y,ArrayMatrix,visited))
+	DFS(m,n,x+1,y,ArrayMatrix,visited));
 	
-	if(is_Valid_Move(x,y-1,ArrayMatrix,visited))
-	DFS(x,y-1);
+	if(is_Valid_Move(m,n,x,y-1,ArrayMatrix,visited))
+	DFS(m,n,x,y-1,ArrayMatrix,visited);
 }
 
 int main()
@@ -44,7 +44,7 @@ int main()
 		for(int j=1;j<=n;j++)
 		if(ArrayMatrix[i][j] =='P' && visited[i][j] == false)
 		{
-			DFS(i,j,ArrayMatrix,visited);
+			DFS(m,n,i,j,ArrayMatrix,visited);
 			count++;			
 		}
 	}

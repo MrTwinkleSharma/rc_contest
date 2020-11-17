5d34924a20d7779e5dc234ed68b4569186e6dc70
Mr. Twinkle Sharma <smtwinkle451@gmail.com>
2020-11-18 04:45:21 +0530

    Updated Variables

--- Businees&War/busnwar.cpp
+++ Businees&War/busnwar.cpp
@@ -1,52 +1,66 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+//Dimension of city
 int m,n;
-	
+
+//Just a flag to be used for storing record of place which was visited
 bool visited[10001][10001];
+
+//To Represent Mumbai City
 char ArrayMatrix[10001][10001];
+
+//Four Direction possible inside city, here we are considering city as matrix
 int move_in_x[] = {-1,0,1,0};
 int move_in_y[] = {0,1,0,-1};
 
-
-bool is_Valid_Move(int x, int y)
+//Function checks whether it is possible to move to that position inside city
+bool is_Valid_Move(int nextPosition_x, int nextPosition_y)
 {
-	if (x<1 || x>m || y<1 || y>n) return false;
+	if (nextPosition_x<1 || nextPosition_x>m || nextPosition_y<1 || nextPosition_y>n) return false;
 	
-	if(visited[x][y] == true || ArrayMatrix[x][y]== '#') return false;
+	if(visited[nextPosition_x][nextPosition_y] == true || ArrayMatrix[nextPosition_x][nextPosition_y]== 'G') return false;
 	
 	return true;
 }
 
-void DFS(int x, int y)
+//Searching for the places 
+void SearchingDFS(int nextPosition_x, int nextPosition_y)
 {
-	visited[x][y] = true;
+	visited[nextPosition_x][nextPosition_y] = true;
 	for(int i=0;i<4;i++)
 	{
-		if(is_Valid_Move(x + move_in_x[i], y + move_in_y[i]))
-			DFS(x + move_in_x[i], y+ move_in_y[i]);	
+		if(is_Valid_Move(nextPosition_x + move_in_x[i], nextPosition_y + move_in_y[i]))
+			SearchingDFS(nextPosition_x + move_in_x[i], nextPosition_y+ move_in_y[i]);	
 	}
 }
 
 int main()
 {
+	//Input for Dimension
 	cin>>m>>n;
+	
+	//Input for Graphical Representation 
 	for(int i=1;i<=m;i++)
 	{
 		for(int j=1;j<=n;j++)
 		cin>>ArrayMatrix[i][j];		
 	}
 	
-	int count = 0;
+	//Counter to be set for finding minimum number of place to be visited
+	int Minimum_Number_Places = 0;
+	
+	//Main loop of algorithm
 	for(int i=1;i<=m;i++)
 	{
 		for(int j=1;j<=n;j++)
-		if(ArrayMatrix[i][j] =='.' && visited[i][j] == false)
+		if(ArrayMatrix[i][j] =='P' && visited[i][j] == false)
 		{
-			DFS(i,j);
-			count++;			
+			SearchingDFS(i,j);
+			Minimum_Number_Places++;			
 		}
 	}
-	cout<<count;
+	cout<<Minimum_Number_Places<<"\n";
 	return 0;
 }
 
